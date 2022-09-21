bfc9e1189ac8a263554a10cda8b1cf9e6ce26c7e
lucidbard <lucidbard@users.github.com>
2022-09-21 18:02:39 +0000

    Sep 21, 2022, 2:02 PM

--- src/FastAccessStateTracker/Tests/behavior.test.ts
+++ src/FastAccessStateTracker/Tests/behavior.test.ts
@@ -16,7 +16,7 @@ beforeAll( async () => {
     await createConnection({
         "name": "test",
         "type": "sqlite",
-        "database": "../database/test.db", 
+        "database": ":memory:", 
         "synchronize": true,
         "logging": true,
         "dropSchema": true,
