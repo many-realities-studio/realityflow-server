6690bb4dabf252703dbf11bb0cd614fb7f425075
lucidbard <lucidbard@users.github.com>
2022-09-21 18:26:13 +0000

    Sep 21, 2022, 2:26 PM

--- src/ORMCommands/Commands.test.ts
+++ src/ORMCommands/Commands.test.ts
@@ -87,7 +87,7 @@ describe("User", () => {
         let returnedUser = await conn.manager.save(newUser)
 
         // act
-      UserOperations.deleteUser(newUser.Username)
+        UserOperations.deleteUser(newUser.Username)
       
         let check = await getConnection(process.env.NODE_ENV).createQueryBuilder().
             select("user").
