381f7774704bd6f39a8d4c6f9838d7222e5b6c07
lucidbard <lucidbard@users.github.com>
2022-09-27 14:48:21 +0000

    Sep 27, 2022, 10:48 AM

--- src/ORMCommands/behaviour.ts
+++ src/ORMCommands/behaviour.ts
@@ -17,7 +17,7 @@ export class BehaviourOperations
      * @param BehaviourInfo new Behaviour info
      * @param projectId project for association
      */
-  public static async CreateBehaviour(BehaviourInfo: FlowBehaviour[])
+  public static async CreateBehaviour(BehaviourInfo: any[])
     {
         // I hate ORMs
         return await getConnection(process.env.NODE_ENV)
